54b74c863437c78d6c66bfad1c124d173e5f3a8b
jingni1115 <kj227777@naver.com>
2023-11-14 20:43:12 +0900

    init Xcode

---
+++ CustomVideoCamera-Swift/CustomVideoCamera-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CustomVideoCamera-Swift
+//
+//  Created by 김지은 on 2023/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
